f44f4ab9f94c570d09b50a091eb5cdb6e1c400b7
mzacki <zacki.mateusz@gmail.com>
2018-08-17 04:13:17 +0000

    concurrent prime checker

---
+++ src/main/java/edu/ant/algorithms/concurrency/PrimeChecker.java
@@ -0,0 +1,35 @@
+package edu.ant.algorithms.concurrency;
+
+import edu.ant.algorithms.math.prime.BruteForcePrimeChecker;
+import edu.ant.algorithms.utils.logger.LoggingService;
+
+public class PrimeChecker implements Runnable {
+
+    private static final String PRIME = "PRIME";
+    private static final String COMPOSITE = "COMPOSITE";
+    private final long n;
+    private final long pause;
+
+    public PrimeChecker(long nLimit, long pause) {
+        this.n = nLimit;
+        this.pause = pause;
+    }
+
+    @Override
+    public void run() {
+        for (int i = 1; i <= n; i++) {
+            try {
+                Thread.sleep(pause);
+            } catch (InterruptedException e) {
+                LoggingService.getLogger().error(e.getMessage());
+                Thread.currentThread().interrupt();
+            }
+
+            if (BruteForcePrimeChecker.check(i)) {
+                LoggingService.logTwoArgs(i, PRIME);
+            } else {
+                LoggingService.logTwoArgs(i, COMPOSITE);
+            }
+        }
+    }
+}
