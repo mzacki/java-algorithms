d7567dc857587964e5189968e93552a0d260b48b
mzacki <zacki.mateusz@gmail.com>
2018-09-04 15:15:18 +0000

    functional upgrade

--- src/main/java/edu/ant/algorithms/concurrency/streams/CustomFunctional.java
+++ src/main/java/edu/ant/algorithms/concurrency/streams/CustomFunctional.java
@@ -7,7 +7,7 @@ public interface CustomFunctional {
 
     void display(Object... args);
 
-    // event fuctional interface can still have default, already definied method
+    // even fuctional interface can still have default, already definied method
     default void displayError(Exception e) {
         LoggingService.getLogger()
                 .error(e.getMessage());
