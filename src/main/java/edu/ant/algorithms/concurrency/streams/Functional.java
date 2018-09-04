83818ffc470b1b0814cdfd22798127c04a60a156
mzacki <zacki.mateusz@gmail.com>
2018-09-04 15:14:23 +0000

    functional upgrade

--- src/main/java/edu/ant/algorithms/concurrency/streams/Functional.java
+++ src/main/java/edu/ant/algorithms/concurrency/streams/Functional.java
@@ -7,6 +7,7 @@ import java.util.List;
 import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.IntConsumer;
+import java.util.function.LongFunction;
 import java.util.function.Predicate;
 import java.util.function.ToIntFunction;
 import java.util.function.UnaryOperator;
@@ -25,36 +26,40 @@ public class Functional {
 
     public static void main(String[] args) {
 
+        // implemented own custom functional interface with void() method
+        CustomFunctional customFunctional = LoggingService::logMessage;
+
         // Consumer within Consumer
-        Consumer<List<String>> stringConsumer = p -> p.forEach(Functional::display);
+        Consumer<List<String>> stringConsumer = p -> p.forEach(customFunctional::display);
         stringConsumer.accept(STRING_LIST);
 
         // IntConsumer, specialized type of Consumer interface
         // accepts argument, returns nothing
-        IntConsumer integerConsumer = Functional::display;
+        IntConsumer integerConsumer = customFunctional::display;
         integerConsumer.accept(1992);
 
         // Function accepts arg of first parametrized type and returns another (or the same) type
         // here list of Integers is transformed into stream of Integers with easy sum up.
         Function<List<Integer>, Stream<Integer>> function = Collection::stream;
-        display(function.apply(INT_LIST).reduce(0, Integer::sum));
+        customFunctional.display(function.apply(INT_LIST).reduce(0, Integer::sum));
 
         // Unary, simplier than Function<String, String>
         UnaryOperator<String> unary = String::toUpperCase;
-        display(unary.apply("Once upon a time there were beez in the beehive...."));
+        customFunctional.display(unary.apply("Once upon a time there were beez in the beehive...."));
+
+        LongFunction<Long> longFunction = l -> l * l;
+        customFunctional.display(longFunction.apply(1243));
 
         // String to Int Function
         ToIntFunction<String> intFunction = String::length;
-        display("Password length: " + intFunction.applyAsInt("MyImpossibleToBreakPass"));
+        customFunctional.display("Password length: " + intFunction.applyAsInt("MyImpossibleToBreakPass"));
 
         // Predicate - tests condition
         Predicate<String> predicate = s -> s.length() >= 8;
-        display("Password has at least 8 chars: " + predicate.test("Pass123"));
+
+        customFunctional.display("Password has at least 8 chars: " + predicate.test("Pass123"));
 
     }
 
-    private static void display(Object... args) {
-        LoggingService.logMessage(args);
-    }
 
 }
