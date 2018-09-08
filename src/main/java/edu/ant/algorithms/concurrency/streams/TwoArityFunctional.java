6b7242e9268ab42cfd98631e1f314cb66bb7d46c
mzacki <zacki.mateusz@gmail.com>
2018-09-08 10:35:57 +0000

    bifunctional implementation

--- src/main/java/edu/ant/algorithms/concurrency/streams/TwoArityFunctional.java
+++ src/main/java/edu/ant/algorithms/concurrency/streams/TwoArityFunctional.java
@@ -1,9 +1,16 @@
 package edu.ant.algorithms.concurrency.streams;
 
+import edu.ant.algorithms.collections.generic.KeyValuePair;
 import edu.ant.algorithms.utils.logger.LoggingService;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.util.function.BiFunction;
+import java.util.stream.Collectors;
 import org.slf4j.LoggerFactory;
 
 /**
@@ -30,6 +37,27 @@ public class TwoArityFunctional {
 
         players.forEach(LoggingService::logTwoArgs);
 
+        Map<Long, String> newMap = new HashMap<>();
+        players.forEach(newMap::put);
+
+        // bi-functional used within randomize()
+        randomize(players).forEach(LoggingService::logTwoArgs);
+
+    }
+
+    private static<T, U> Map<T, U> randomize(Map<T, U> input) {
+        List<T> keyList = new ArrayList<>(input.keySet());
+        List<U> valueList = new ArrayList<>(input.values());
+        Collections.shuffle(keyList);
+        Collections.shuffle(valueList);
+        Map<T, U> map = new HashMap<>();
+        BiFunction<T, U, U> biFunction = map::put;
+
+        for (int i = 0; i < keyList.size(); i++) {
+            biFunction.apply(keyList.get(i), valueList.get(i));
+        }
+
+        return map;
     }
 
 }
