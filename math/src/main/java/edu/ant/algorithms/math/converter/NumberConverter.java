45ecc60038266a391e9e199b2ed8f9d040949b47
mzacki <zacki.mateusz@gmail.com>
2021-02-22 17:37:59 +0100

    decimal to base converter

--- math/src/main/java/edu/ant/algorithms/math/converter/NumberConverter.java
+++ math/src/main/java/edu/ant/algorithms/math/converter/NumberConverter.java
@@ -4,14 +4,16 @@ public final class NumberConverter {
 
     private NumberConverter() {}
 
+    /**
+     * power of two value for given bit position:
+     * current value of nth bit in binary number (if bit == 1)
+     * starting from the rightmost: 2 to power 0 equals 1 (if bit == 1)
+     * then 2 to 1st = 2, then 2 to 2nd = 4 and so on.
+     * @param binary String representation of binary number
+     * @return decimal number (long)
+     * */
     public static long binaryToDecimal(String binary) {
         long decimal = 0;
-        /**
-         * power of two value for given bit position:
-         * current value of nth bit in binary number (if bit == 1)
-         * starting from the rightmost: 2 to power 0 equals 1 (if bit == 1)
-         * then 2 to 1st = 2, then 2 to 2nd = 4 and so on.
-         * */
         long bitValue = 1;
 
         for (int bitPosition = binary.length() - 1; bitPosition >= 0; bitPosition--) {
@@ -23,14 +25,16 @@ public final class NumberConverter {
         return decimal;
     }
 
+    /**
+     * divide by 2, remainder is least significant bit (LSB)
+     * then divide quotient by 2, remainder is next LSB
+     * repeat until quotient == 1
+     * @param decimal (long)
+     * @return String of binary number
+     */
     public static String decimalToBinary(long decimal) {
         StringBuilder binaryBuilder = new StringBuilder();
-        /**
-         * divide by 2, remainder is least significant bit (LSB)
-         * then divide quotient by 2, remainder is next LSB
-         * repeat until quotient == 1
-         * */
-        while(decimal > 0) {
+        while (decimal > 0) {
             binaryBuilder.insert(0, decimal % 2);
             decimal /= 2;
         }
@@ -39,6 +43,8 @@ public final class NumberConverter {
                 "0" : binaryBuilder.toString();
     }
 
+
+    // supports power of n <= 10
     public static long baseToDecimal(String number, int n) {
         long decimal = 0;
         int nthPower = 1;
@@ -50,6 +56,20 @@ public final class NumberConverter {
         }
         return decimal;
     }
+
+    // similar to decimal to binary but n power customizable
+    // supports power of n <= 10
+    public static String decimalToBase(long decimal, int n) {
+        StringBuilder numberBuilder = new StringBuilder();
+        while (decimal > 0) {
+            numberBuilder.insert(0, decimal % n);
+            decimal /= n;
+        }
+        return numberBuilder.toString().equals("") ?
+                "0" : numberBuilder.toString();
+    }
+
+    // TODO HEX converter (power of n > 10)
 
 
 
