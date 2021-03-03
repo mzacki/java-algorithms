7dddabb24f5f49dba07653b2932036dbb618b93f
mzacki <zacki.mateusz@gmail.com>
2021-03-03 17:52:19 +0100

    counting sort test

---
+++ algo/src/test/groovy/edu/ant/algorithms/algo/sort/counting/CountingSortTest.groovy
@@ -0,0 +1,36 @@
+package edu.ant.algorithms.algo.sort.counting
+
+import spock.lang.Specification
+
+class CountingSortTest extends Specification {
+
+    def "should sort input"() {
+        expect:
+        CountingSort countingSort = new CountingSort(input as int[])
+        result == countingSort.sort() as ArrayList<Integer>
+
+        where:
+        input | result
+        [13] | [13]
+        [13, -22] | [-22, 13]
+        [56, 33, -8, 456] | [-8, 33, 56, 456]
+        [13, 4, 0, -7, -22] | [-22, -7, 0, 4, 13]
+        [13, 4, 0, -7, -22, 156] | [-22, -7, 0, 4, 13, 156]
+        [4, 7, 1, 2, 9, 34, 76, 12, 0] | [0, 1, 2, 4, 7, 9, 12, 34, 76]
+    }
+
+    def "should reverse sort input"() {
+        expect:
+        CountingSort countingSort = new CountingSort(input as int[])
+        result == countingSort.reverseSort() as ArrayList<Integer>
+
+        where:
+        input | result
+        [13] | [13]
+        [13, 2] | [13, 2]
+        [56, 33, 8, 456] | [456, 56, 33, 8]
+        [13, 4, 0, 7, 22] | [22, 13, 7, 4, 0]
+        [13, 4, 0, 7, 22, 156] | [156, 22, 13, 7, 4, 0]
+        [4, 7, 1, 2, 9, 34, 76, 12, 0] | [76, 34, 12, 9, 7, 4, 2, 1, 0]
+    }
+}
