a60af1387214aadc1c589bff6fcfd889b4a4ce21
mzacki <zacki.mateusz@gmail.com>
2021-03-08 18:18:52 +0100

    main

---
+++ data/src/main/java/edu/ant/algorithms/data/structures/list/linked/ExampleOfUse.java
@@ -0,0 +1,32 @@
+package edu.ant.algorithms.data.structures.list.linked;
+
+public class ExampleOfUse {
+
+    public static void main(String[] args) {
+        Item item1 = new Item(1L, "item one");
+        Item item2 = new Item(2L, "item two");
+        Item item3 = new Item(3L, "item three");
+        Item item4 = new Item(4L, "item four");
+
+        SingleLinkedList list = new SingleLinkedList();
+        list.add(item1);
+        list.add(item2);
+        list.add(item3);
+        list.add(item4);
+
+        list.log();
+        list.remove();
+        list.log();
+
+        HeadAndTailLinkedList headAndTailLinkedList = new HeadAndTailLinkedList();
+        headAndTailLinkedList.add(item1);
+        headAndTailLinkedList.add(item2);
+        headAndTailLinkedList.add(item3);
+        headAndTailLinkedList.add(item4);
+
+        headAndTailLinkedList.log();
+        headAndTailLinkedList.remove();
+        headAndTailLinkedList.log();
+    }
+
+}
