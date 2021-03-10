6d2208956418e5228088557da02ab916d54f3a39
mzacki <zacki.mateusz@gmail.com>
2021-03-10 19:04:32 +0100

    doubly linked list

---
+++ data/src/main/java/edu/ant/algorithms/data/structures/list/linked/doubly/DoubleNode.java
@@ -0,0 +1,44 @@
+package edu.ant.algorithms.data.structures.list.linked.doubly;
+
+import edu.ant.algorithms.data.structures.list.linked.Item;
+
+public class DoubleNode {
+
+    private Item item;
+    private DoubleNode consequent;
+    private DoubleNode antecedent;
+
+    public DoubleNode(Item item) {
+        this.item = item;
+    }
+
+    public Item getItem() {
+        return item;
+    }
+
+    public void setItem(Item item) {
+        this.item = item;
+    }
+
+    public DoubleNode getConsequent() {
+        return consequent;
+    }
+
+    public void setConsequent(DoubleNode consequent) {
+        this.consequent = consequent;
+    }
+
+    public DoubleNode getAntecedent() {
+        return antecedent;
+    }
+
+    public void setAntecedent(DoubleNode antecedent) {
+        this.antecedent = antecedent;
+    }
+
+    @Override
+    public String toString() {
+        return item.toString();
+    }
+
+}
