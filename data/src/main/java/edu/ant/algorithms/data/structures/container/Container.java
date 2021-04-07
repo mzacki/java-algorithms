86b2b4d2985f7ea152d9e941d8af04246fb572c2
mzacki <zacki.mateusz@gmail.com>
2021-04-07 19:32:58 +0200

    container as record

--- data/src/main/java/edu/ant/algorithms/data/structures/container/Container.java
+++ data/src/main/java/edu/ant/algorithms/data/structures/container/Container.java
@@ -1,12 +1,6 @@
 package edu.ant.algorithms.data.structures.container;
 
-public class Container<T> {
-
-    private final T item;
-
-    public Container(T item) {
-        this.item = item;
-    }
+public record Container<T>(T item) {
 
     public T getItem() {
         return item;
