aa66843a536edf4daf6291376b715afc6f41fd9e
mzacki <38253922+mzacki@users.noreply.github.com>
2022-02-08 07:19:22 +0100

    groovy and spock intro

--- algo/src/test/groovy/edu/ant/algorithms/intro/GroovyExample.groovy
+++ algo/src/test/groovy/edu/ant/algorithms/intro/GroovyExample.groovy
@@ -2,5 +2,14 @@ package edu.ant.algorithms.intro
 
 class GroovyExample {
 
-    String content
+    // def with variables: wildcard for variable type
+    def content
+
+    // def with methods: wildcard for return type
+    def transform() {
+        // return keyword is not necessary
+        // last evaluated statement is being returned
+        "Content transformed: transformed " + content
+    }
+
 }
