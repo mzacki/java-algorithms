87d1510d4d4f2512a19f917989f6f065cea25331
mzacki <38253922+mzacki@users.noreply.github.com>
2022-04-07 12:53:58 +0200

    array shuffle

---
+++ algo/src/main/java/edu/ant/algorithms/algo/shuffle/ArrayShuffle2.java
@@ -0,0 +1,49 @@
+package edu.ant.algorithms.algo.shuffle;
+
+import edu.ant.patterns.utils.logger.LoggingService;
+import java.util.Arrays;
+import java.util.Random;
+
+public class ArrayShuffle2 {
+
+
+    /**
+     * for given int n
+     * return all int values from 1 to n (inclusive)
+     * in random order
+     * */
+
+    private static final Random RANDOM = new Random();
+
+    public static void main(String[] args) {
+        LoggingService.logMsg(Arrays.toString(getRandomized(5)));
+    }
+
+    private static int[] getRandomized(int n) {
+
+        int[] array = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            array[i] = i + 1;
+        }
+
+        return shuffle(array);
+    }
+
+    private static int[] shuffle(int[] array) {
+        // shuffles only with subsequent indices - which have not been shuffled before
+        for (int i = 0; i < array.length; i++) {
+            int randomIndex = i + RANDOM.nextInt(array.length - i);
+            swap(array, i, randomIndex);
+        }
+
+        return array;
+    }
+
+    private static void swap(int[] array, int currentIndex, int randomIndex) {
+        int temp = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temp;
+    }
+
+}
