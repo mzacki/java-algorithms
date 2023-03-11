6c56c3205895f179586947d5e1fa7705858ce0fc
mzacki <38253922+mzacki@users.noreply.github.com>
2023-03-11 14:01:44 +0100

    EndX

---
+++ puzzle/src/main/java/edu/ant/algorithms/puzzle/solutions/codingbat/EndX.java
@@ -0,0 +1,26 @@
+package edu.ant.algorithms.puzzle.solutions.codingbat;
+
+public class EndX {
+
+    /***
+     * Given a string, compute recursively a new string where all the lowercase 'x' chars have been moved to the end of the string.
+     * endX("xxre") → "rexx"
+     * endX("xxhixx") → "hixxxx"
+     * endX("xhixhix") → "hihixxx"
+     * */
+
+    public String endX(String str) {
+
+        if (str.length() < 1) {
+            return str;
+        }
+
+        if (str.charAt(0) == 'x') {
+            return endX(str.substring(1)) + "x";
+        } else {
+            return str.charAt(0) + endX(str.substring(1));
+        }
+
+    }
+
+}
